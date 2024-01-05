3e6ee65ebe4af59096279f1ca0f43f91715b61b1
RedaMjibel <reda_mjibel@hotmail.com>
2024-01-04 17:39:05 -0800

    test

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def hbnb_filters():
+    """
+    returns a rendered html template
+    using the data from states and amenities
+    """
+    states = storage.all('State').value()
+    citites = storage.all('City').value()
+    amenities = storage.all('Amenity').value
+    return render_template('10-hbnb_filters.html',
+                           states=states, cities=cities, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
